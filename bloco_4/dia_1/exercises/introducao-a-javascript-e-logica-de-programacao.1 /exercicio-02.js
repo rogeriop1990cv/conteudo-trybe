f534c016c39d721cfc923ea4c9198b0e3815f5b1
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-05-19 16:13:09 -0300

    2. Faça um programa que retorne ... comparados. - OK

---
+++ bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao.1 /exercicio-02.js
@@ -0,0 +1,12 @@
+let numero1 = 40;
+let numero2 = 2;
+
+
+// 2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+if(numero1 > numero2){
+    console.log(numero1);
+}
+else{
+    console.log(numero2);
+}
