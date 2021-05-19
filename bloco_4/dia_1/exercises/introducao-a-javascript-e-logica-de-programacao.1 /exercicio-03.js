e76dac1207de82280663adea20eaf1d92e3eefa5
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-05-19 16:34:02 -0300

    3. Faça um programa que retorne o maior de três números ... comparados.

---
+++ bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao.1 /exercicio-03.js
@@ -0,0 +1,20 @@
+let numero1 = 10;
+let numero2 = 0;
+let numero3 = 20;
+
+//3. Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+let maior = 0
+
+if(maior < numero1){
+    maior = numero1
+}
+
+if(maior < numero2){
+    maior = numero2
+}
+
+if(maior < numero3){
+    maior = numero3
+}
+
+console.log(maior);
