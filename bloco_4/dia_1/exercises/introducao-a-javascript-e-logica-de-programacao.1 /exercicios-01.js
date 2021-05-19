52d8b4bffe934533922f559604940632d9af4c36
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-05-19 16:00:50 -0300

    1. Faça...comparados - OK

---
+++ bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao.1 /exercicios-01.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 5;
+
+// Adição (a + b)
+console.log(a + b);
+// Subtração (a - b)
+console.log(a - b);
+// Multiplicação (a * b)
+console.log(a * b);
+// Divisão (a / b)
+console.log(a / b);
+// Módulo (a % b)
+console.log(a % b);
