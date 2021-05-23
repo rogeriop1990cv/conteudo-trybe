b13b3bef53baf19c26027b2262dd54a4205270c1
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-05-23 14:29:16 -0300

    Exercicio 4 atualizando e 5 pra fazer

--- Fundamentos do Desenvolvimento Web/bloco_4/dia_3/exercises/exercicio4.js
+++ Fundamentos do Desenvolvimento Web/bloco_4/dia_3/exercises/exercicio4.js
@@ -1,12 +1,13 @@
-let n = 5;
+let n = 3;
 let meio = Math.floor(n / 2);
-let l = 1;
+let l = 0;
 
+if (n % 2 === 1) {
+    l = 1;
+}
+
+console.log(`n = ${n}`);
 for (let linha = 0; linha < n; linha++) {
-    if(linha < meio){
-        console.log(''.repeat(meio - 1));
-        continue
-    }
     console.log(' '.repeat(meio).concat('*'.repeat(l)).concat(' '.repeat(meio)))
     if (meio > 0) {
         meio -= 1;
