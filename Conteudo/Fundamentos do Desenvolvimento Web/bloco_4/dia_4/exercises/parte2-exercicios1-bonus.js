2d4761a2a43f9b066501fd710a95dac9966a0121
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-05-26 15:24:39 -0300

    Exercicios Imcompleto

--- Conteudo/Fundamentos do Desenvolvimento Web/bloco_4/dia_4/exercises/parte2-exercicios1-bonus.js
+++ Conteudo/Fundamentos do Desenvolvimento Web/bloco_4/dia_4/exercises/parte2-exercicios1-bonus.js
@@ -1,24 +1,15 @@
-// Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
-// Valor de teste: 'trybe' e 'be'
-// Valor esperado no retorno da função: true
-// verificaFimPalavra('trybe', 'be') ;
-// Retorno esperado: true
-// verificaFimPalavra('joaofernando', 'fernan') ;
-// Retorno esperado: false
-
-function verificaFimPalavra(word, ending){
-  let indexWord = (word.length - ending.length);
-  if(ending.length > word.length){
-    return 'error'
+function toNumRomanosNumDec(valor) {
+  // Obejto com a chave e o valor dela
+  let converte = []
+  const romanos = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
   }
-
-  for(let i = 0; i < ending.length; i++){
-    if(ending[i] !== word[indexWord]){
-      return false
-    }
-    indexWord += 1
-  }
-  return true
+  return soma
 }
-
-console.log(verificaFimPalavra('trybe', 'eeb'));
+console.log(toNumRomanosNumDec('IV'))
