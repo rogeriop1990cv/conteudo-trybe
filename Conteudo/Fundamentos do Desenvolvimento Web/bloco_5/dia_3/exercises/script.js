1997ed1a49b11c929c6ec946cf15743383c3f681
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-05-29 01:45:09 -0300

    Exercicio 1 completoCalendario Tryber - ok

---
+++ Conteudo/Fundamentos do Desenvolvimento Web/bloco_5/dia_3/exercises/script.js
@@ -0,0 +1,41 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+// Exercício 1:
+
+function createDaysOfTheMonth(){
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+  const daysListItem = document.querySelector('#days')
+
+  for(let day of dezDaysList){
+    const li = document.createElement('li');
+    li.className = 'day'
+    li.innerText = day;
+    if([24, 25, 31].includes(day)){
+      li.className = 'day holiday'
+      li.innerText = day;
+    }
+    
+    if([4, 11, 18, 25].includes(day)){
+      li.className = 'day friday'
+      li.innerText = day;
+    }
+
+    daysListItem.appendChild(li)
+    console.log(li);
+  }
+}
+
+createDaysOfTheMonth()
