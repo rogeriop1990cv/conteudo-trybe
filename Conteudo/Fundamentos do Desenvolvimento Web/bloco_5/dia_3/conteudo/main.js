873f66798529cb694207fb5ec69fe3c1cd67c088
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-05-29 00:56:37 -0300

    Fnalizando exemplo do conteudo

--- Conteudo/Fundamentos do Desenvolvimento Web/bloco_5/dia_3/conteudo/main.js
+++ Conteudo/Fundamentos do Desenvolvimento Web/bloco_5/dia_3/conteudo/main.js
@@ -3,30 +3,41 @@ const divDois = document.getElementById('divDois');
 const divTres = document.getElementById('divTres');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
-
+divDois.className
 /*
  Copie esse arquivo e edite apenas ele;
  Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+
 1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
  Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-com a classe 'tech';
+com a classe 'tech'
+
  Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-redirecione para alguma página;
+redirecione para alguma página
+
 1. Que tal redirecionar para seu portifólio?
  Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-a cor do mesmo;
+a cor do mesmo
+
 
 Segue abaixo um exemplo do uso de event.target:
 */
 
-function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
-}
+document.body.addEventListener('dblclick', (element) => {
+  element.target.className = 'tech'
+  console.log(element.target.innerText);
+  element.target.innerText = input.value;
+})
 
-divUm.addEventListener('dblclick', resetText);
-// Não precisa passar o parâmetro dentro do addEventListener. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'divUm'.
+document.body.addEventListener('dblclick', (element) => {
+  element.target.className = 'tech'
+})
+document.body.addEventListener('mouseout', (element) => {
+  if(element.target.className === 'tech'){
+    element.target.removeAttribute('class')
+  }
+})
+
+myWebpage.addEventListener('mouseover', () => { myWebpage.style.color = 'red' })
+myWebpage.addEventListener('mouseout', () => { myWebpage.style.color = 'white' })
+myWebpage.addEventListener('click', () => { window.open('https://google.com', '_blank') })
