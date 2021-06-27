5892ae318f7703368ec6ba2642c2b311ccefc085
Rogerio P. Da Silva <rogeriop1990@gmail.com>
2021-06-27 15:34:03 -0300

    Resolvido exercicio 2 do conteudo.

--- Fundamentos do Desenvolvimento Web/bloco_7/dia_1/exercises/exercicio2.js
+++ Fundamentos do Desenvolvimento Web/bloco_7/dia_1/exercises/exercicio2.js
@@ -1,5 +1,5 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-// Seu código aqui.
+const sortList = (lista) => lista.sort((a, b) => a - b);
 
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+console.log(sortList(oddsAndEvens)); // será necessário alterar essa linha 😉
