03d9d0db06cc867db0e60adcc737bdf3dcf800c5
Rogerio P. Da Silva <rogeriop1990@gmail.com>
2021-06-27 17:14:33 -0300

    Resolvido o  exercicio 4 da parte 2

---
+++ Fundamentos do Desenvolvimento Web/bloco_7/exercicioPartII-4.js
@@ -0,0 +1,12 @@
+// Função 1.
+
+const stringDeterminada =  "Tryber x aqui!";
+const skills = ['html', 'css', 'js', 'python', 'gimp']
+
+const changeXtoString = (word) => {
+    return stringDeterminada.replace(' x ', ` ${word} `)
+}
+
+
+
+// função 2.
