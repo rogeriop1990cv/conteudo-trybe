d995cf8a81cf40f04dd96f9d85de86166b7dc716
Rogerio P. Da Silva <rogeriop1990@gmail.com>
2021-06-27 20:05:24 -0300

    add code in file

--- Fundamentos do Desenvolvimento Web/bloco_7/dia_2/exercises/part2/exercises.js
+++ Fundamentos do Desenvolvimento Web/bloco_7/dia_2/exercises/part2/exercises.js
@@ -1,21 +1,20 @@
 const lesson1 = {
-    materia: 'Matemática',
-    numeroEstudantes: 20,
-    professor: 'Maria Clara',
-    turno: 'manhã',
-  };
-  
-  const lesson2 = {
-    materia: 'História',
-    numeroEstudantes: 20,
-    professor: 'Carlos',
-  };
-  
-  const lesson3 = {
-    materia: 'Matemática',
-    numeroEstudantes: 10,
-    professor: 'Maria Clara',
-    turno: 'noite',
-  };
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
 
-  
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
