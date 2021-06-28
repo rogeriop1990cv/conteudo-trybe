ef2ba0a5f95b8397c0e2d0fce0f09d26a954e856
Rogerio P. Da Silva <rogeriop1990@gmail.com>
2021-06-28 03:44:34 -0300

    Requisito 1 - Part 1 Ok.

--- Fundamentos do Desenvolvimento Web/bloco_7/dia_3/exercises/part1/sum.js
+++ Fundamentos do Desenvolvimento Web/bloco_7/dia_3/exercises/part1/sum.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { EROFS } = require('constants');
 
 function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
@@ -8,4 +9,9 @@ function sum(a, b) {
   return a + b;
 }
 
-// implemente seus testes aqui
+assert.strictEqual(sum(4, 5), 9);
+assert.strictEqual(sum(0, 0), 0);
+
+// https://www.geeksforgeeks.org/nodejs-assert-throws-function/
+assert.throws(() => {sum(4, '1')});
+assert.throws(() => {sum(4, '1')}, /^Error: parameters must be numbers$/ );
