c21e3bd6558010be4555be7e9214c4f0c9ee1c02
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-07-15 00:43:50 -0300

    myRemove.spec.js - Completa

---
+++ Fundamentos do Desenvolvimento Web/bloco_10/dia_1/exercises/part1/myRemove.spec.js
@@ -0,0 +1,33 @@
+const { myRemove } = require('./unitestexercices')
+
+describe('A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o \
+ elemento item caso ele exista no array ', () => {
+  it(' Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    const array = [1, 2, 3, 4];
+    expect(myRemove(array, 3)).toEqual([1, 2, 4]);
+  })
+
+  it(' Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    const array = [1, 2, 3, 4];
+    expect(myRemove(array, 3)).not.toEqual([1, 2, 3, 4]);
+  })
+
+  it(' Verifique se o array passado por parâmetro não sofreu alterações', () => {
+    const array = [1, 2, 3, 4];
+    const arrayReturn = myRemove(array, 3)
+
+    expect(array).toEqual([1, 2, 3, 4]);
+  })
+
+  it(' Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    const array = [1, 2, 3, 4];
+    expect(myRemove(array, 5)).toEqual([1, 2, 3, 4]);
+  })
+
+
+ })
+
+
+
+
+
