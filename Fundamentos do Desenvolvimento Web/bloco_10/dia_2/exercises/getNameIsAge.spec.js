4b9007cc1f43d3804785355813332f9d35800207
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-07-17 15:35:13 -0300

    Exercicio 6.1 ok

---
+++ Fundamentos do Desenvolvimento Web/bloco_10/dia_2/exercises/getNameIsAge.spec.js
@@ -0,0 +1,34 @@
+const animals = [
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+const findAnimalByName = (name) => {
+  return animals.find(animal => animal.name === name)
+};
+
+const getAnimal = (name) => {
+  const animal = findAnimalByName(name);
+  return animal || 'Nenhum animal com esse nome!';
+};
+
+describe('Testando promise - findAnimalByName', () => {
+  describe('Quando existe o animal com o nome procurado', () => {
+    test('Retorne o objeto do animal', () => {
+      expect.assertions(1);
+      return getAnimal('Dorminhoco').then(animal => {
+        expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+      });
+    });
+  });
+
+  describe('Quando não existe o animal com o nome procurado', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return getAnimal('Bob').catch(error =>
+        expect(error).toEqual('Nenhum animal com esse nome!')
+      );
+    });
+  });
+});
