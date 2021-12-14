16bcc48e5292fa6b100f309d7ace7fe273e08a9d
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-12-14 16:36:46 -0300

    Exercicio do Conteudo 03 - Ok

--- 03 - Desenvolvimento Back-end/bloco_21/dia_2/conteudo/01_Como_utilizar_o_INNER_JOIN.sql
+++ 03 - Desenvolvimento Back-end/bloco_21/dia_2/conteudo/01_Como_utilizar_o_INNER_JOIN.sql
@@ -5,10 +5,7 @@ SELECT
   `film`.film_id
 FROM
   sakila.actor AS `actor`
-INNER JOIN
-  sakila.film_actor AS `film`
-  ON `actor`.actor_id = `film`.actor_id;
-
+  INNER JOIN sakila.film_actor AS `film` ON `actor`.actor_id = `film`.actor_id;
 -- 02
 SELECT
   `staff`.first_name,
@@ -16,12 +13,28 @@ SELECT
   `address`.address
 FROM
   sakila.staff AS `staff`
-INNER JOIN
-  sakila.address AS `address` 
-  ON `staff`.address_id = `address`.address_id
-;
+  INNER JOIN sakila.address AS `address` ON `staff`.address_id = `address`.address_id;
+-- 03
+SELECT
+  `customer`.customer_id,
+  `customer`.first_name AS `name`,
+  `customer`.email,
+  `address`.address_id,
+  `address`.address
+FROM
+  sakila.customer AS `customer`
+  INNER JOIN sakila.address AS `address` ON `address`.address_id = `customer`.address_id
+ORDER BY
+  `name` DESC
+LIMIT
+  100;
 
+SELECT
+  *
+FROM
+  sakila.customer;
 
-
-SELECT * FROM staff;
-SELECT * FROM address;
+SELECT
+  *
+FROM
+  address;
