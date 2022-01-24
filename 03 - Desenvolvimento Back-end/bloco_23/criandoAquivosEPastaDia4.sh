2c957a24e0c6624cb5c2d06dfe076cb45d11a1b6
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-01-24 22:56:33 +0000

    criando todas as configurações

---
+++ 03 - Desenvolvimento Back-end/bloco_23/criandoAquivosEPastaDia4.sh
@@ -0,0 +1,13 @@
+mkdir controllers
+mkdir services
+mkdir tests
+mkdir tests/controllers
+mkdir tests/services
+mkdir tests/models
+
+touch tests/controllers/movieController.test.js
+touch tests/services/movieService.test.js
+touch tests/models/movieModel.test.js
+touch controllers/movieController.js
+touch services/movieService.js
+touch index.js 
